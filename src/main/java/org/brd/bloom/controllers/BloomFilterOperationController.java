28e2f58bcae8ad3438ecccd99b595aff410f33b3
unknown <swapnajit25@gmail.com>
2021-05-30 21:29:04 +0530

    Added Cross origin

--- src/main/java/org/brd/bloom/controllers/BloomFilterOperationController.java
+++ src/main/java/org/brd/bloom/controllers/BloomFilterOperationController.java
@@ -3,11 +3,13 @@ package org.brd.bloom.controllers;
 import org.brd.bloom.engine.BloomFilter;
 import org.brd.bloom.exceptions.BloomFilterException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class BloomFilterOperationController
 {
 	@Autowired
